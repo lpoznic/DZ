579884a0f22f63206bac1845cdea5308fb37c697
lpoznic <lpoznic@net.hr>
2020-10-24 23:00:12 +0200

    DZ1

---
+++ DZ1/ClassLibrary1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Episode
+    {
+        public int ViewerCount;
+        public double AverageScore;
+        public double MaxScore;
+
+        public int GetViewerCount() { return ViewerCount; }
+        public double GetAverageScore() { return AverageScore; }
+        public double GetMaxScore() { return MaxScore; }
+
+
+        public Episode()
+        {
+            ViewerCount = 0;
+            AverageScore = 0;
+            MaxScore = 0;
+        }
+
+        public Episode(int viewerCount, double averageScore, double maxScore)
+        {
+            this.ViewerCount = viewerCount;
+            this.AverageScore = averageScore;
+            this.MaxScore = maxScore;
+        }
+
+        public void AddView(double Score)
+        {
+            ViewerCount++;
+            AverageScore += Score;
+
+            if (Score > MaxScore)
+            {
+                MaxScore = Score;
+            }
+
+        }
+
+    }
+
+}
